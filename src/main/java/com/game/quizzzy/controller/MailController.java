3e1de4929ea75e26726b5f86348eeff962e1178a
ignatIgnatov <ignatov_rs@abv.bg>
2024-04-03 15:42:40 +0300

    refactoring

--- src/main/java/com/game/quizzzy/controller/MailController.java
+++ src/main/java/com/game/quizzzy/controller/MailController.java
@@ -8,7 +8,6 @@ import com.game.quizzzy.service.MailService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
